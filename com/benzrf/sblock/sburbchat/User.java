fdbb610475ce60f5c40994d2fc02ef5fcc587346
benzrf <benzrf@benzrf.com>
2013-01-10 12:47:27 -0500

    Added a necessary import. DAMMIT VIM WHY DON'T YOU HAVE AUTO-IMPORTS

--- com/benzrf/sblock/sburbchat/User.java
+++ com/benzrf/sblock/sburbchat/User.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
