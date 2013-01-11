e1efdc0014fb34aef5fc5f680b873d5dbb30d485
benzrf <benzrf@benzrf.com>
2013-01-11 00:44:06 -0500

    Switched pinging back to ghast shooting because notes weren't working.

--- com/benzrf/sblock/sburbchat/User.java
+++ com/benzrf/sblock/sburbchat/User.java
@@ -126,10 +126,10 @@ public class User
 			if (ChatColor.stripColor(s).indexOf(this.getName()) > 3)
 			{
 				this.pthis.sendMessage(c.getPrefix() + ChatColor.BLUE + "{!} " + ChatColor.WHITE + s);
-				this.pthis.sendBlockChange(this.pthis.getLocation().add(0, 2, 0), Material.NOTE_BLOCK, (byte) 0);
-				this.pthis.playNote(this.pthis.getLocation().add(0, 2, 0), (byte) 0, (byte) 6);
-				this.pthis.sendBlockChange(this.pthis.getLocation().add(0, 2, 0), this.pthis.getLocation().add(0, 2, 0).getBlock().getType(), this.pthis.getLocation().add(0, 2, 0).getBlock().getData());
-				// this.pthis.playEffect(this.pthis.getLocation(), Effect.GHAST_SHOOT, 0);
+				// this.pthis.sendBlockChange(this.pthis.getLocation().add(0, 2, 0), Material.NOTE_BLOCK, (byte) 0);
+				// this.pthis.playNote(this.pthis.getLocation().add(0, 2, 0), (byte) 0, (byte) 6);
+				// this.pthis.sendBlockChange(this.pthis.getLocation().add(0, 2, 0), this.pthis.getLocation().add(0, 2, 0).getBlock().getType(), this.pthis.getLocation().add(0, 2, 0).getBlock().getData());
+				this.pthis.playEffect(this.pthis.getLocation(), Effect.GHAST_SHOOT, 0);
 			}
 			else
 			{
